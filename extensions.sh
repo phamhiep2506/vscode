cb6beea711138cdd0e710080c65d93756906de13
phamhiep2506 <phamhiep25062k@gmail.com>
2024-06-06 15:47:15 +0700

    vscode vim

---
+++ extensions.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+code --install-extension christian-kohler.path-intellisense
+code --install-extension drcika.apc-extension
+code --install-extension editorconfig.editorconfig
+code --install-extension jdinhlife.gruvbox
+code --install-extension miguelsolorio.symbols
+code --install-extension ms-dotnettools.csdevkit
+code --install-extension ms-dotnettools.csharp
+code --install-extension ms-dotnettools.vscode-dotnet-runtime
+code --install-extension usernamehw.errorlens
+code --install-extension vscodevim.vim
