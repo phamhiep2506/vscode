f63184e7c036919748533224495ae38422c5baed
phamhiep2506 <phamhiep25062k@gmail.com>
2024-06-10 19:06:10 +0700

    update

--- extensions.sh
+++ extensions.sh
@@ -5,8 +5,6 @@ code --install-extension drcika.apc-extension
 code --install-extension editorconfig.editorconfig
 code --install-extension jdinhlife.gruvbox
 code --install-extension miguelsolorio.symbols
-code --install-extension ms-dotnettools.csdevkit
-code --install-extension ms-dotnettools.csharp
-code --install-extension ms-dotnettools.vscode-dotnet-runtime
 code --install-extension usernamehw.errorlens
 code --install-extension vscodevim.vim
+code --install-extension ms-dotnettools.csdevkit
